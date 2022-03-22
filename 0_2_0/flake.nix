a3737ef7c269bbebdf7c2b6297f2f6cd907bf886
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:59 -0300

    chore: re-index ../nimpkgs/c/commandant/0_2_0

--- 0_2_0/flake.nix
+++ 0_2_0/flake.nix
@@ -9,8 +9,8 @@
   
   inputs.src-commandant-0_2_0.flake = false;
   inputs.src-commandant-0_2_0.owner = "casey-SK";
-  inputs.src-commandant-0_2_0.ref   = "refs/tags/0.2.0";
-  inputs.src-commandant-0_2_0.repo  = "commandant";
+  inputs.src-commandant-0_2_0.ref   = "0_2_0";
+  inputs.src-commandant-0_2_0.repo  = "commandant.git";
   inputs.src-commandant-0_2_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
